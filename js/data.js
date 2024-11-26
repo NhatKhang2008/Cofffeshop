5dab2257776bbe46707482b82f0d06603980e8f7
Khang <lehoainhatkhang2008@gmail.com>
2024-11-26 20:14:41 +0700

    k.

--- js/data.js
+++ js/data.js
@@ -10,7 +10,7 @@ export const categories = [
         imgUrl:
           "https://tocotocotea.com/wp-content/uploads/2024/10/Sua-Tuoi-Nep-Cam.png",
         description:
-          "Hồng Trà Mận Quế Hoa Khổng Lồ là sự hòa quyện giữa hương vị đậm đà của hồng trà kết hợp hoàn hảo cùng mứt mận Mộc Châu chín đỏ mọng nước đã được tách hạt chỉ có ở ToCoToCo với vị chua ngọt và thạch quế hoa mềm mướt, vị ngọt nhẹ, điểm bên trong là hoa mộc quế cùng các lát chanh vàng tươi mát tạo ra trải nghiệm đầy thú vị và lôi cuốn.",
+          "Sự kết hợp yến mạch dẻo thơm cùng sữa tươi béo nhẹ mang đến một món uống vừa bổ dưỡng vừa thơm ngon.",
       },
       {
         id: 2,
@@ -18,20 +18,24 @@ export const categories = [
         price: 18000,
         categorys: ["Món nổi bật", "Cà phê"],
         imgUrl:
-          "https://tocotocotea.com/wp-content/uploads/2024/10/Sua-Tuoi-Nep-Cam.png",
+          "https://tocotocotea.com/wp-content/uploads/2023/07/9501a1967827a879f136.jpg",
+        description:
+          "Được làm từ cà phê robusta, đậm vị và hương thơm nồng nàn quyến rũ",
       },
       {
         id: 3,
         name: "Cà phê sữa đá",
-        price: 20000,
+        price: 18000,
         categorys: ["Món nổi bật", "Cà phê"],
         imgUrl:
-          "https://tocotocotea.com/wp-content/uploads/2024/10/Sua-Tuoi-Nep-Cam.png",
+          "https://tocotocotea.com/wp-content/uploads/2023/07/170b2e9cf72d27737e3c.jpg",
+        description:
+          "Sự hòa quyện giữa cà phê đậm vị robusta xen lẫn vào đó là vị ngọt béo của sữa đặc và dậy lên một mùi hương quyến rũ",
       },
     ],
   },
   {
-    name: "trà sữa",
+    name: "Trà sữa",
     fruit_items: [
       {
         id: 1,
@@ -41,27 +45,27 @@ export const categories = [
         imgUrl:
           "https://tocotocotea.com/wp-content/uploads/2024/10/Sua-Tuoi-Nep-Cam.png",
         description:
-          "Hồng Trà Mận Quế Hoa Khổng Lồ là sự hòa quyện giữa hương vị đậm đà của hồng trà kết hợp hoàn hảo cùng mứt mận Mộc Châu chín đỏ mọng nước đã được tách hạt chỉ có ở ToCoToCo với vị chua ngọt và thạch quế hoa mềm mướt, vị ngọt nhẹ, điểm bên trong là hoa mộc quế cùng các lát chanh vàng tươi mát tạo ra trải nghiệm đầy thú vị và lôi cuốn.",
+          "Sự kết hợp yến mạch dẻo thơm cùng sữa tươi béo nhẹ mang đến một món uống vừa bổ dưỡng vừa thơm ngon.",
       },
       {
         id: 1,
-        name: "Sữa tươi yến mạch",
-        price: 39000,
+        name: "Ô Long Đào Lê Tây Bắc",
+        price: 25000,
         categorys: ["Món nổi bật", "Special menu"],
         imgUrl:
-          "https://tocotocotea.com/wp-content/uploads/2024/10/Sua-Tuoi-Nep-Cam.png",
+          "https://tocotocotea.com/wp-content/uploads/2024/08/O-Long-Dao-Le-Tay-Bac.png",
         description:
-          "Hồng Trà Mận Quế Hoa Khổng Lồ là sự hòa quyện giữa hương vị đậm đà của hồng trà kết hợp hoàn hảo cùng mứt mận Mộc Châu chín đỏ mọng nước đã được tách hạt chỉ có ở ToCoToCo với vị chua ngọt và thạch quế hoa mềm mướt, vị ngọt nhẹ, điểm bên trong là hoa mộc quế cùng các lát chanh vàng tươi mát tạo ra trải nghiệm đầy thú vị và lôi cuốn.",
+          "Sự kết hợp hoàn hảo của lê Tây Bắc và đào tiên má hồng giòn ngọt tự nhiên, thưởng thức cùng trà ô long khói vô cùng thanh lọc, bổ dưỡng",
       },
       {
         id: 1,
-        name: "Sữa tươi yến mạch",
-        price: 39000,
+        name: "Trà sữa dâu tây",
+        price: 34000,
         categorys: ["Món nổi bật", "Special menu"],
         imgUrl:
-          "https://tocotocotea.com/wp-content/uploads/2024/10/Sua-Tuoi-Nep-Cam.png",
+          "https://tocotocotea.com/wp-content/uploads/2021/01/Tra-Sua-Dau-Tay-2-copy.jpg",
         description:
-          "Hồng Trà Mận Quế Hoa Khổng Lồ là sự hòa quyện giữa hương vị đậm đà của hồng trà kết hợp hoàn hảo cùng mứt mận Mộc Châu chín đỏ mọng nước đã được tách hạt chỉ có ở ToCoToCo với vị chua ngọt và thạch quế hoa mềm mướt, vị ngọt nhẹ, điểm bên trong là hoa mộc quế cùng các lát chanh vàng tươi mát tạo ra trải nghiệm đầy thú vị và lôi cuốn.",
+          "Hương vị chua ngọt hài hòa đặc trưng của Dâu tây và trà thanh mát",
       },
     ],
   },
@@ -74,7 +78,9 @@ export const categories = [
         price: 18000,
         categorys: ["Món nổi bật", "Cà phê"],
         imgUrl:
-          "https://tocotocotea.com/wp-content/uploads/2024/10/Sua-Tuoi-Nep-Cam.png",
+          "https://tocotocotea.com/wp-content/uploads/2023/07/9501a1967827a879f136.jpg",
+        description:
+          "Được làm từ cà phê robusta, đậm vị và hương thơm nồng nàn quyến rũ",
       },
       {
         id: 3,
@@ -82,15 +88,19 @@ export const categories = [
         price: 20000,
         categorys: ["Món nổi bật", "Cà phê"],
         imgUrl:
-          "https://tocotocotea.com/wp-content/uploads/2024/10/Sua-Tuoi-Nep-Cam.png",
+          "https://tocotocotea.com/wp-content/uploads/2023/07/170b2e9cf72d27737e3c.jpg",
+        description:
+          "Sự hòa quyện giữa cà phê đậm vị robusta xen lẫn vào đó là vị ngọt béo của sữa đặc và dậy lên một mùi hương quyến rũ",
       },
       {
         id: 3,
-        name: "Cà phê đá",
-        price: 20000,
+        name: "Cheese Milk Coffee",
+        price: 30000,
         categorys: ["Món nổi bật", "Cà phê"],
         imgUrl:
-          "https://tocotocotea.com/wp-content/uploads/2024/10/Sua-Tuoi-Nep-Cam.png",
+          "https://tocotocotea.com/wp-content/uploads/2021/11/cheese-milk-coffee.png",
+        description:
+          "Sự hòa quyện giữa cà phê robusta đậm vị cùng sữa béo thơm, vị ngọt nhẹ nhàng từ topping sương sáo mềm mịn",
       },
     ],
   },
